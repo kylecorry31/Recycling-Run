e302b459b50623b6276d235f9a0849be05a2ac24
Kyle Corry <kylecorry31@gmail.com>
2017-02-15 11:38:16 -0500

    Added ice cream container

--- game-master.js
+++ game-master.js
@@ -1,38 +1,43 @@
 var GAME_MASTER = {
     items: [{
-        name: "Newspaper",
-        image: "assets/trashItems/newspaper.png",
-        recyclable: true,
-        uid: 0
-    }, {
-        name: "Pizza",
-        image: "assets/trashItems/pizza.png",
-        recyclable: false,
-        uid: 1
-    }, {
-        name: "Soda Can",
-        image: "assets/trashItems/can.png",
-        recyclable: true,
-        uid: 2
-    }, {
-        name: "Cardboard Box",
-        image: "assets/trashItems/box.png",
-        recyclable: true,
-        uid: 3
-    }, {
-        name: "Receipt",
-        image: "assets/trashItems/receipt.png",
-        recyclable: false,
-        uid: 4
-    }, {
-        name: "Clamshell Packaging",
-        image: "assets/trashItems/clamshell.png",
-        recyclable: false,
-        uid: 5
-    }, {
-        name: "Dunkin Bag",
-        image: "assets/trashItems/dunkinBag.png",
-        recyclable: true,
-        uid: 6
-    }] //dunkin cup, glass bottles, dunkin lids, ice cream container, greasy pizza boxes, lightbulbs, batteries
+            name: "Newspaper",
+            image: "assets/trashItems/newspaper.png",
+            recyclable: true,
+            uid: 0
+        }, {
+            name: "Pizza",
+            image: "assets/trashItems/pizza.png",
+            recyclable: false,
+            uid: 1
+        }, {
+            name: "Soda Can",
+            image: "assets/trashItems/can.png",
+            recyclable: true,
+            uid: 2
+        }, {
+            name: "Cardboard Box",
+            image: "assets/trashItems/box.png",
+            recyclable: true,
+            uid: 3
+        }, {
+            name: "Receipt",
+            image: "assets/trashItems/receipt.png",
+            recyclable: false,
+            uid: 4
+        }, {
+            name: "Clamshell Packaging",
+            image: "assets/trashItems/clamshell.png",
+            recyclable: false,
+            uid: 5
+        }, {
+            name: "Dunkin Bag",
+            image: "assets/trashItems/dunkinBag.png",
+            recyclable: true,
+            uid: 6
+        }, {
+            name: "Ice Cream Container",
+            image: "assets/trashItems/iceCream.png",
+            recyclable: false,
+            uid: 7
+        }] //dunkin cup, glass bottles, dunkin lids, ice cream container, greasy pizza boxes, lightbulbs, batteries
 };
