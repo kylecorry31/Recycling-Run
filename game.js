3412d2a5a413f570365f717b4ff22d709f6f9f49
Kyle Corry <kylecorry31@gmail.com>
2017-02-15 12:23:58 -0500

    Fix instructions off screen

--- game.js
+++ game.js
@@ -236,7 +236,7 @@ function drawKeyHint() {
     fill(255);
     textSize(24);
     textAlign(RIGHT);
-    text("Move with the arrow keys\nPress 'Space' to drop items", WIDTH - 25, HEIGHT - 5);
+    text("Move with the arrow keys\nPress 'Space' to drop items", WIDTH - 25, HEIGHT - 50);
 }
 
 function drawScore() {
